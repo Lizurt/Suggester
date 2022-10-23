499f457efb0e38d16cfd3262b54deeaaeb876701
Lizurt <lizard.x@mail.ru>
2022-10-23 13:23:49 +0300

    fixed the error missing node bug

--- src/main/java/com/example/arangui/arango/hints/ArangoQueryDefaultAnalyser.java
+++ src/main/java/com/example/arangui/arango/hints/ArangoQueryDefaultAnalyser.java
@@ -16,13 +16,12 @@ import java.util.*;
 
 public class ArangoQueryDefaultAnalyser implements ArangoQueryAnalyser {
     public final static Random HINTS_GENERATOR_RANDOM = new Random(123456789);
-    // fixme: "insert {a:5} i" generates error node in the start (an empty "INTO" node)
     public List<String> getHints(ArangoParserRules.QueryContext queryContext) {
         ParseTree lastDeepestNode = getLastDeepestNode(queryContext);
-        ParseTree nodeForHints = lastDeepestNode;
+        ParseTree parentNodeForHints = lastDeepestNode;
         LinkedList<ParseTree> writtenNodesInIncompleteToken = new LinkedList<>();
         if (lastDeepestNode instanceof ErrorNode) {
-            nodeForHints = lastDeepestNode.getParent();
+            parentNodeForHints = lastDeepestNode.getParent();
             // firstly let's check its parent in case if there are other error nodes
             ParseTree nodeParent = lastDeepestNode.getParent();
             for (int i = nodeParent.getChildCount() - 1; i >= 0; i--) {
@@ -30,17 +29,36 @@ public class ArangoQueryDefaultAnalyser implements ArangoQueryAnalyser {
                     writtenNodesInIncompleteToken.addFirst(nodeParent.getChild(i));
                 }
             }
-        } else if (lastDeepestNode instanceof TerminalNode) {
-            nodeForHints = lastDeepestNode.getParent();
-            ParseTree nodeParent = lastDeepestNode.getParent();
-            for (int i = nodeParent.getChildCount() - 1; i >= 0; i--) {
-                if (nodeParent.getChild(i) instanceof TerminalNode) {
-                    writtenNodesInIncompleteToken.addFirst(nodeParent.getChild(i));
+        } else  {
+            // genius antlr generates empty error nodes sometimes instead of counting next nodes as the error one.
+            // We have to check all the previous nodes to find a missing one if it exists
+            ParseTree possibleErrorMissingNode = tryFindErrorMissingNodeInSiblingsAndAncestors(lastDeepestNode);
+            if (possibleErrorMissingNode == null) {
+                // alright no missing nodes, but this node is probably haven't been written completely, let's try to
+                // find hints for it
+                if (lastDeepestNode instanceof TerminalNode) {
+                    parentNodeForHints = lastDeepestNode.getParent();
+                    ParseTree nodeParent = lastDeepestNode.getParent();
+                    for (int i = nodeParent.getChildCount() - 1; i >= 0; i--) {
+                        if (nodeParent.getChild(i) instanceof TerminalNode) {
+                            writtenNodesInIncompleteToken.addFirst(nodeParent.getChild(i));
+                        }
+                    }
+                }
+            } else {
+                // alright there were error missing nodes, let's assume a user is writing that missing node but their
+                // input was misidentified as some other nodes
+                parentNodeForHints = possibleErrorMissingNode.getParent();
+                int nodeIndex = getNodeIndexInParentNode(possibleErrorMissingNode);
+                for (int i = nodeIndex; i < possibleErrorMissingNode.getChildCount(); i++) {
+                    writtenNodesInIncompleteToken.addAll(
+                            getAllChildesOf(possibleErrorMissingNode.getParent().getChild(i))
+                    );
                 }
             }
         }
         String userWrittenTokenPart = nodesToString(writtenNodesInIncompleteToken);
-        Rule ruleForHints = ArangoParserRules.getRuleByContextClass(nodeForHints.getClass());
+        Rule ruleForHints = ArangoParserRules.getRuleByContextClass(parentNodeForHints.getClass());
         if (ruleForHints == null) {
             throw new RuntimeException("RuleForHints is null which shouldn't happen");
         }
@@ -68,6 +86,61 @@ public class ArangoQueryDefaultAnalyser implements ArangoQueryAnalyser {
         }
 
         return hints;
+    }
+
+    private List<ParseTree> getAllChildesOf(ParseTree parent) {
+        if (parent.getChildCount() == 0) {
+            return Collections.emptyList();
+        }
+        ArrayList<ParseTree> result = new ArrayList<>();
+        for (int i = 0; i < parent.getChildCount(); i++) {
+            result.add(parent.getChild(i));
+            result.addAll(getAllChildesOf(parent.getChild(i)));
+        }
+        return result;
+    }
+
+    private int getNodeIndexInParentNode(ParseTree child) {
+        ParseTree parent = child.getParent();
+        if (parent == null) {
+            return -1;
+        }
+        for (int i = 0; i < parent.getChildCount(); i++) {
+            if (child == parent.getChild(i)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    private ParseTree tryFindErrorMissingNodeInSiblingsAndAncestors(ParseTree node) {
+        if (isNodeErrorMissing(node)) {
+            return node;
+        }
+        ParseTree parent = node.getParent();
+        if (parent == null) {
+            return null;
+        }
+        for (int i = parent.getChildCount() - 1; i >= 0; i--) {
+            // yes we're duplicating the check above for the argument node, but I haven't found any good solution for it
+            if (isNodeErrorMissing(parent.getChild(i))) {
+                return parent.getChild(i);
+            }
+        }
+        return tryFindErrorMissingNodeInSiblingsAndAncestors(parent);
+    }
+
+    private boolean isNodeErrorMissing(ParseTree node) {
+        if (node instanceof ErrorNode possibleMissingNode) {
+            if (possibleMissingNode.getSymbol().getStartIndex() < 0
+                    && possibleMissingNode.getSymbol().getStopIndex() < 0
+            ) {
+                return true;
+            }
+            // not sure if we can reach this line. Let's throw an exception in case if we do to debug this later
+            throw new RuntimeException("The node was an ErrorNode but not a missing one. Unexpected behaviour");
+        }
+        return false;
     }
 
     private List<Tree> getFirstHintableNodesInRule(Rule ruleForHints) {
